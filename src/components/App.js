ee975a6fed15527982d2d7113ba6cf097728849e
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2020-08-03 13:59:24 +0500

    feat(context): контекст текущего пользователя и карточек

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,34 @@ import Footer from './Footer.js';
 import Main from './Main.js'
 import PopupWithForm from './PopupWithForm.js'
 import ImagePopup from './ImagePopup.js'
+import api from '../utils/Api.js';
+import { CurrentUserContext } from '../contexts/CurrentUserContext.js'
+import { InitialCardsContext } from '../contexts/InitialCardsContext.js'
+
 
 function App() {
+
+  //информация о текущем пользователе
+  const [currentUser, setCorrentUser] = React.useState({});
+  const [initialCards, setInitialCards] = React.useState([]);
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false);
   const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false);
   const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = React.useState(false);
+  //хук для поп-ап окна при нажатии на картинку
   const [selectedCard, setSelectedCard] = React.useState({
     isOpen: false,
     name: '',
     link: ''
   })
+
+  React.useEffect(() => {
+    Promise.all([api.getInfoUser(), api.getInitialCards()])
+    .then(([userData, cardsFromApi]) => {
+      setCorrentUser(userData);
+      setInitialCards(cardsFromApi);
+    })
+    .catch(err => console.log(err))
+  }, [])
 
   function handleCardClick(name, link) {
     setSelectedCard({
@@ -46,29 +64,33 @@ function App() {
 
   return (
     <>
-      <Header />
-      <Main 
-      onEditProfile={handleEditProfileClick} onAddPlace={handleAddPlaceClick} onEditAvatar={handleEditAvatarClick} onCardClick={handleCardClick} 
-      />
-      <PopupWithForm name="edit" title="Редактировать профиль" inputSignature="Сохранить" isOpen={isEditProfilePopupOpen} onClose={closeAllPopups}>
-            <input name="name" type="text" className="popup__input popup__input-name" id='name-input'   required pattern="[A-Za-zА-Яа-я -]{2,40}" placeholder="Имя" />
-            <span className="popup__input-error" id="name-input-error"></span>
-            <input name="about" type="text" className="popup__input popup__input-signature"   id='signature-input' required minLength='2' maxLength='200' placeholder="О себе" />
-            <span className="popup__input-error" id="signature-input-error"></span>
-      </PopupWithForm>
-      <PopupWithForm name="place" title="Новое место" inputSignature="Создать" isOpen={isAddPlacePopupOpen} onClose={closeAllPopups} >
-            <input name="name" type="text" className="popup__input popup__input-name" id='name-input'   required pattern="[A-Za-zА-Яа-я -]{1,30}" placeholder="Название" />
-            <span className="popup__input-error" id="name-input-error"></span>
-            <input name="link" type="url" className="popup__input popup__input-signature"   id='signature-input' required placeholder="Ссылка на картинку" />
-            <span className="popup__input-error" id="signature-input-error"></span>
-      </PopupWithForm>
-      <PopupWithForm name="avatar" title="Обновить аватар" inputSignature="Сохранить" isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups} >
-            <input name="avatar" type="url" className="popup__input popup__input-signature"   id='signature-input' required placeholder="Ссылка на картинку" />
-            <span className="popup__input-error" id="signature-input-error"></span>
+      <CurrentUserContext.Provider value={currentUser}>
+      <InitialCardsContext.Provider value={initialCards}>
+        <Header />
+        <Main 
+        onEditProfile={handleEditProfileClick} onAddPlace={handleAddPlaceClick} onEditAvatar={handleEditAvatarClick} onCardClick={handleCardClick} 
+        />
+        <PopupWithForm name="edit" title="Редактировать профиль" inputSignature="Сохранить" isOpen={isEditProfilePopupOpen} onClose={closeAllPopups}>
+              <input name="name" type="text" className="popup__input popup__input-name" id='name-input'   required pattern="[A-Za-zА-Яа-я -]{2,40}" placeholder="Имя" />
+              <span className="popup__input-error" id="name-input-error"></span>
+              <input name="about" type="text" className="popup__input popup__input-signature"   id='signature-input' required minLength='2' maxLength='200' placeholder="О себе" />
+              <span className="popup__input-error" id="signature-input-error"></span>
         </PopupWithForm>
-      <PopupWithForm name="delete" title="Вы уверены?" inputSignature="Да" />
-      <ImagePopup card={selectedCard} onClose={closeAllPopups} />
-      <Footer />
+        <PopupWithForm name="place" title="Новое место" inputSignature="Создать" isOpen={isAddPlacePopupOpen} onClose={closeAllPopups} >
+              <input name="name" type="text" className="popup__input popup__input-name" id='name-input'   required pattern="[A-Za-zА-Яа-я -]{1,30}" placeholder="Название" />
+              <span className="popup__input-error" id="name-input-error"></span>
+              <input name="link" type="url" className="popup__input popup__input-signature"   id='signature-input' required placeholder="Ссылка на картинку" />
+              <span className="popup__input-error" id="signature-input-error"></span>
+        </PopupWithForm>
+        <PopupWithForm name="avatar" title="Обновить аватар" inputSignature="Сохранить" isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups} >
+              <input name="avatar" type="url" className="popup__input popup__input-signature"   id='signature-input' required placeholder="Ссылка на картинку" />
+              <span className="popup__input-error" id="signature-input-error"></span>
+          </PopupWithForm>
+        <PopupWithForm name="delete" title="Вы уверены?" inputSignature="Да" />
+        <ImagePopup card={selectedCard} onClose={closeAllPopups} />
+      </InitialCardsContext.Provider>
+      </CurrentUserContext.Provider>
+        <Footer />
     </>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,40 @@
 import React from 'react'
 
-function Card ({link, name, likes, onCardClick}) {
+function Card ({currentUser, card, onCardClick}) {
+
+  // Определяем, являемся ли мы владельцем текущей карточки
+  const isOwn = card.owner._id === currentUser._id;
+
+  // Создаём переменную, которую после зададим в `className` для кнопки удаления
+  const cardDeleteButtonClassName = (
+    `${isOwn ? 'card__btn-delete' : 'card__btn-delete_hidden'}`
+  );
+
+  // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
+  const isLiked = card.likes.some(like => like._id === currentUser._id);
+
+  // Создаём переменную, которую после зададим в `className` для кнопки лайка
+  const cardLikeButtonClassName = (
+    `${isLiked ? 'btn-image btn-image_like btn-image_like_active' : 'btn-image btn-image_like'}`
+    ); 
+
   function handleClick() {
-    onCardClick(name, link)
+    onCardClick(card.name, card.link)
   }
+
     return (
         <div className="card">
-            <img src={link} alt={`карточка с изображением ${name}`} className="card__image" onClick={handleClick} />
-            <button className="card__btn-delete">
+            <img src={card.link} alt={`карточка с изображением ${card.name}`} className="card__image" onClick={handleClick} />
+            <button className={cardDeleteButtonClassName}>
               <div className="btn-image btn-image_delete"></div>
             </button>
             <div className="card__info">
-              <h2 className="card__title">{name}</h2>
+              <h2 className="card__title">{card.name}</h2>
               <div className="card__block-like">
                 <button className="card__btn-like">
-                  <div className="btn-image btn-image_like"></div>
+                  <div className={cardLikeButtonClassName}></div>
                 </button>
-                <span className="card__count-like">{likes}</span>
+                <span className="card__count-like">{card.likes.length}</span>
               </div>
             </div>
         </div>
